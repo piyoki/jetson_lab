c7c55c6d31e2678c7c7d3c0594bb76e602c21f4d
Chinglong Yu <31861128+yqlbu@users.noreply.github.com>
2020-05-31 18:05:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ sudo apt-get install python3-pip
 pip3 install jupyterlab
 pip3 install --upgrade --force jupyter-console
 export PATH=$PATH:~/.local/bin >> ~/.bashrc
+source ~/.bashrc
 echo "*** Installation finished ***"
 echo "Version: "
 jupyter lab -V

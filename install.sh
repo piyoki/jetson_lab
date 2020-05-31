13ffe4ffc4d93ddfe6661d2c070f4ed2d71976f4
Chinglong Yu <31861128+yqlbu@users.noreply.github.com>
2020-05-31 18:06:24 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo *** Install Jupyter @ Jetson ***
-sudo pip3 install --upgrade pip 
 sudo apt-get install python3-pip
+sudo pip3 install --upgrade pip
 pip3 install jupyterlab
 pip3 install --upgrade --force jupyter-console
-export "PATH=$PATH:~/.local/bin" >> ~/.bashrc
+echo 'export PATH=$PATH:~/.local/bin' >> ~/.bashrc
 source ~/.bashrc
 echo "*** Installation finished ***"
 echo "Version: "
